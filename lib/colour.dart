861320e1c4121c09f0a6f9c2264bb1a644e43046
Salman S <salkuadrat@gmail.com>
2020-09-21 08:14:16 +0700

    dartfmt

--- lib/colour.dart
+++ lib/colour.dart
@@ -3,8 +3,9 @@ library colour;
 import 'package:flutter/material.dart';
 
 class Colour extends Color {
-  Colour(dynamic color, [dynamic x, dynamic y, dynamic z]) : super(getColor(color, x, y, z));
-  
+  Colour(dynamic color, [dynamic x, dynamic y, dynamic z])
+      : super(getColor(color, x, y, z));
+
   static int getColor(dynamic color, [dynamic x, dynamic y, dynamic z]) {
     bool hasX = x != null;
     bool hasY = y != null;
@@ -15,14 +16,14 @@ class Colour extends Color {
     bool noZ = z == null;
 
     // check if Colour only use the fist paramter (color)
-    // it could be a normal color: Colour(0xFF121212) 
-    // or a hex based color: Colour('#FF121212') or Colour('FF121212') 
+    // it could be a normal color: Colour(0xFF121212)
+    // or a hex based color: Colour('#FF121212') or Colour('FF121212')
     // or Colour('#121212') or Colour('121212')
     bool isSingleParams = noX && noY && noZ;
 
-    if(isSingleParams) {
-      if(color is Color) return color.value;
-      if(color is int) return color; // Normal color: Colour(0xFF121212)
+    if (isSingleParams) {
+      if (color is Color) return color.value;
+      if (color is int) return color; // Normal color: Colour(0xFF121212)
       return getColorFromHex(color.toString());
     }
 
@@ -36,11 +37,14 @@ class Colour extends Color {
     bool isDoubleParams = hasX && noY && noZ;
     bool isXOpacity = x is double && x >= 0.0 && x <= 1.0;
 
-    if(isDoubleParams && isXOpacity) {
+    if (isDoubleParams && isXOpacity) {
       Color _color;
-      if(color is Color) _color = color;
-      else if(color is int) _color = Color(color);
-      else _color = Color(getColorFromHex(color.toString()));
+      if (color is Color)
+        _color = color;
+      else if (color is int)
+        _color = Color(color);
+      else
+        _color = Color(getColorFromHex(color.toString()));
       return _color.withOpacity(x).value;
     }
 
@@ -49,7 +53,7 @@ class Colour extends Color {
     bool isTripleParams = hasX && hasY && noZ;
     bool isTripleParamsInteger = color is int && x is int && y is int;
 
-    if(isTripleParams && isTripleParamsInteger) {
+    if (isTripleParams && isTripleParamsInteger) {
       return Color.fromARGB(255, color, x, y).value;
     }
 
@@ -59,18 +63,23 @@ class Colour extends Color {
     // RGBO => Colour(12, 123, 255, 0.25)
     bool isAllParams = hasX && hasY && hasZ;
 
-    if(isAllParams) {
+    if (isAllParams) {
       // if all parameters is int, then it is ARGB => Colour(255, 12, 123, 255)
       bool isARGB = color is int && x is int && y is int && z is int;
 
-      if(isARGB) {
+      if (isARGB) {
         return Color.fromARGB(color, x, y, z).value;
       }
 
       // if the last paramter is double, then it is RGBO => Colour(12, 123, 255, 0.25)
-      bool isRGBO = color is int && x is int && y is int && z is double && z >= 0.0 && z <= 1.0;
+      bool isRGBO = color is int &&
+          x is int &&
+          y is int &&
+          z is double &&
+          z >= 0.0 &&
+          z <= 1.0;
 
-      if(isRGBO) {
+      if (isRGBO) {
         return Color.fromRGBO(color, x, y, z).value;
       }
     }
@@ -85,7 +94,7 @@ class Colour extends Color {
   // Colour('121212')
   static int getColorFromHex(String color) {
     String hex = color.toString().toUpperCase().replaceAll('#', '');
-    if(hex.length == 6) hex = 'FF' + hex;
+    if (hex.length == 6) hex = 'FF' + hex;
     return int.parse(hex, radix: 16);
   }
 }
--- test/colour_test.dart
+++ test/colour_test.dart
@@ -12,8 +12,10 @@ void main() {
   });
 
   test('Normal color with opacity', () {
-    assert(Colour(0xFFFFFFFF, 0.25).value == Color(0xFFFFFFFF).withOpacity(0.25).value);
-    assert(Colour(0x00000000, 0.5).value == Color(0x00000000).withOpacity(0.5).value);
+    assert(Colour(0xFFFFFFFF, 0.25).value ==
+        Color(0xFFFFFFFF).withOpacity(0.25).value);
+    assert(Colour(0x00000000, 0.5).value ==
+        Color(0x00000000).withOpacity(0.5).value);
 
     // use existing color with opacity
     Color color = Color(0xFFFFFFFF);
@@ -25,9 +27,11 @@ void main() {
     assert(Colour('#121212').value == Color(0xFF121212).value);
   });
 
-   test('Normal hex color with opacity', () {
-    assert(Colour('121212', 0.25).value == Color(0xFF121212).withOpacity(0.25).value);
-    assert(Colour('#121212', 0.5).value == Color(0xFF121212).withOpacity(0.5).value);
+  test('Normal hex color with opacity', () {
+    assert(Colour('121212', 0.25).value ==
+        Color(0xFF121212).withOpacity(0.25).value);
+    assert(Colour('#121212', 0.5).value ==
+        Color(0xFF121212).withOpacity(0.5).value);
   });
 
   test('Hex color with transparency', () {
@@ -38,27 +42,36 @@ void main() {
   });
 
   test('Hex color with transparency and opacity', () {
-    assert(Colour('FF121212', 0.25).value == Color(0xFF121212).withOpacity(0.25).value);
-    assert(Colour('B1121212', 0.5).value == Color(0xB1121212).withOpacity(0.5).value);
-    assert(Colour('#B1121212', 0.25).value == Color(0xB1121212).withOpacity(0.25).value);
-    assert(Colour('#00121212', 0.5).value == Color(0x00121212).withOpacity(0.5).value);
+    assert(Colour('FF121212', 0.25).value ==
+        Color(0xFF121212).withOpacity(0.25).value);
+    assert(Colour('B1121212', 0.5).value ==
+        Color(0xB1121212).withOpacity(0.5).value);
+    assert(Colour('#B1121212', 0.25).value ==
+        Color(0xB1121212).withOpacity(0.25).value);
+    assert(Colour('#00121212', 0.5).value ==
+        Color(0x00121212).withOpacity(0.5).value);
   });
 
   test('RGB color', () {
-    assert(Colour(255, 255, 255).value == Color.fromARGB(255, 255, 255, 255).value);
+    assert(Colour(255, 255, 255).value ==
+        Color.fromARGB(255, 255, 255, 255).value);
     assert(Colour(0, 0, 0).value == Color.fromARGB(255, 0, 0, 0).value);
     assert(Colour(123, 24, 80).value == Color.fromARGB(255, 123, 24, 80).value);
   });
 
   test('ARGB color', () {
-    assert(Colour(255, 255, 255, 255).value == Color.fromARGB(255, 255, 255, 255).value);
+    assert(Colour(255, 255, 255, 255).value ==
+        Color.fromARGB(255, 255, 255, 255).value);
     assert(Colour(0, 0, 0, 0).value == Color.fromARGB(0, 0, 0, 0).value);
-    assert(Colour(75, 123, 24, 80).value == Color.fromARGB(75, 123, 24, 80).value);
+    assert(
+        Colour(75, 123, 24, 80).value == Color.fromARGB(75, 123, 24, 80).value);
   });
 
   test('RGBO color', () {
-    assert(Colour(255, 255, 255, 1.0).value == Color.fromRGBO(255, 255, 255, 1.0).value);
+    assert(Colour(255, 255, 255, 1.0).value ==
+        Color.fromRGBO(255, 255, 255, 1.0).value);
     assert(Colour(0, 0, 0, 0.0).value == Color.fromRGBO(0, 0, 0, 0.0).value);
-    assert(Colour(75, 123, 24, 0.25).value == Color.fromRGBO(75, 123, 24, 0.25).value);
+    assert(Colour(75, 123, 24, 0.25).value ==
+        Color.fromRGBO(75, 123, 24, 0.25).value);
   });
 }
